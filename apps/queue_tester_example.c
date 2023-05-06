17a5d4ef63b02f952d0343f1079741c44740aab2
Subhan <subhanbaig209@gmail.com>
2023-05-06 11:43:05 -0700

    testing

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -37,10 +37,87 @@ void test_queue_simple(void)
 	TEST_ASSERT(ptr == &data);
 }
 
+void test_queue_multiple(void)
+{
+	int data1 = 3, data2 = 5, *ptr;
+	queue_t q;
+
+	fprintf(stderr, "*** TEST queue_multiple ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data1);
+	queue_enqueue(q, &data2);
+	queue_dequeue(q, (void**)&ptr);
+	TEST_ASSERT(ptr == &data1);
+	queue_dequeue(q, (void**)&ptr);
+	TEST_ASSERT(ptr == &data2);
+}
+
+/* Dequeue empty */
+void test_queue_empty(void)
+{
+    int *ptr;
+    queue_t q;
+
+    fprintf(stderr, "*** TEST queue_empty ***\n");
+
+    q = queue_create();
+    TEST_ASSERT(queue_dequeue(q, (void**)&ptr) == -1);
+}
+
+
+/*
+void test_queue_delete_single(void)
+{
+	int data = 3;
+	queue_t q;
+
+	fprintf(stderr, "*** TEST queue_delete_single ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	TEST_ASSERT(queue_delete(q, &data) == 0);
+	TEST_ASSERT(queue_length(q) == 0);
+}
+*/
+
+void test_queue_delete_multiple(void)
+{
+	int data1 = 3, data2 = 5, data3 = 7;
+	queue_t q;
+
+	fprintf(stderr, "*** TEST queue_delete_multiple ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data1);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	TEST_ASSERT(queue_delete(q, &data2) == 0);
+	TEST_ASSERT(queue_length(q) == 2);
+}
+
+void test_queue_destroy_empty(void)
+{
+    queue_t q;
+
+    fprintf(stderr, "*** TEST queue_destroy_empty ***\n");
+
+    q = queue_create();
+    TEST_ASSERT(queue_destroy(q) == 0);
+}
+
+
+
+
 int main(void)
 {
 	test_create();
 	test_queue_simple();
+	test_queue_multiple();
+	test_queue_empty();
+	/*test_queue_delete_single();*/
+	test_queue_delete_multiple();
+	test_queue_destroy_empty();
 
 	return 0;
 }
