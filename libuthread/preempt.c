efe0e81263f4e6f5eb8de17337625c9476c411c7
Subhan <subhanbaig209@gmail.com>
2023-05-11 23:13:24 -0700

    added comments

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -17,53 +17,60 @@
  */
 #define HZ 100
 
-sigset_t sig;
-struct sigaction sa;
-//struct sigaction old_sa;
-struct itimerval timer;
-//struct itimerval old_timer;
+sigset_t sig; // hold a set of signals that can be blocked or unblocked
+struct sigaction sa; // used to configure and manage signal actions for a process
+struct itimerval timer; // used to specify the amount of time to wait before sending a signal
+
+// This function handles the SIGVTALRM signal and calls uthread_yield() to allow the current thread to yield the CPU
 
 void preempt_handler(int sigg) {
 	if (sigg == SIGVTALRM)
 		uthread_yield();
 }
 
+// This function disables preemption by blocking the SIGVTALRM signal by using sigprocmask
 void preempt_disable(void) {
 	sigemptyset(&sig);
 	sigaddset(&sig, SIGVTALRM);
 	sigprocmask(SIG_BLOCK, &sig, NULL);
 }
 
+// This function enables preemption by unblocking the SIGVTALRM signal by using sigprocmask
 void preempt_enable(void) {
-        sigemptyset(&sig);
-        sigaddset(&sig, SIGVTALRM);
+    sigemptyset(&sig);
+    sigaddset(&sig, SIGVTALRM);
 	sigprocmask(SIG_UNBLOCK, &sig, NULL);
-
 }
 
+/* This function starts preemption by setting up a SIGVTALRM signal handler, 
+setting a timer to send the signal repeatedly, and enabling the signal */
 void preempt_start(bool preempt) {
-	
+	// Set the SIGVTALRM signal handler to preempt_handler()
 	if (preempt) {
-		sa.sa_handler = preempt_handler;
-		sigemptyset(&sa.sa_mask);
-		sa.sa_flags = 0;
-        	sigaction(SIGVTALRM, &sa, NULL);
 
-        	timer.it_value.tv_sec = 0;
+		sa.sa_handler = preempt_handler;
+		sigemptyset(&sa.sa_mask);  // clear the sa_mask signal set
+		sa.sa_flags = 0;
+        sigaction(SIGVTALRM, &sa, NULL); // set the signal action for SIGVTALRM to sa
+
+		// set the intervals and delays of the timer
+        timer.it_value.tv_sec = 0;
 		timer.it_value.tv_usec = 1000000 / HZ; // 100 Hz
 		timer.it_interval.tv_sec = 0;
-        	timer.it_interval.tv_usec = 1000000 / HZ; // 100 Hz
-        	setitimer(ITIMER_VIRTUAL, &timer, NULL);
-    	}
-    	else {
-        	return;
-    	}
+        timer.it_interval.tv_usec = 1000000 / HZ; // 100 Hz
+		// set the timer to virtual mode and start it
+        setitimer(ITIMER_VIRTUAL, &timer, NULL);
+    }
+    else {
+		// if preemption is disabled, return without doing anything
+        return;
+    }
 }
 
 void preempt_stop(void) {
 	preempt_disable();
-
+	// set the sa_flags to SA_RESETHAND to reset the signal handler for SIGVTALRM to its default behavior
 	sa.sa_flags = SA_RESETHAND;
+	// reset the signal handler for SIGVTALRM to its default behavior
 	sigaction(SIGVTALRM, &sa, NULL);
 }
-
--- libuthread/queue.c
+++ libuthread/queue.c
@@ -1,25 +1,26 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <stdio.h>
-
 #include "queue.h"
 
-
+// structure to define the node of a queue
 struct Node {
-	void* data;
-	struct Node* next;
+	void* data; // pointer to the data element of the node
+	struct Node* next; // pointer to the next node in the queue
 };
 
+// structure to define a queue, including pointer to head of queue and pointer to end of queue, and length of queue
 struct queue {
 	struct Node* head;
 	struct Node* end;
 	int len;
 };
 
+// function to create a new queue and initialize its parameters
 queue_t queue_create(void)
 {
+	// allocating memory for the new queue, and set its head and end to NULL
 	struct queue* q = malloc(sizeof(struct queue));
 	if (q == NULL)
 		return NULL;
@@ -29,6 +30,7 @@ queue_t queue_create(void)
 	return q;
 }
 
+// function to destroy a queue and free its allocated memory
 int queue_destroy(queue_t queue)
 {
 	// check if queue is not null and empty
@@ -42,55 +44,62 @@ int queue_destroy(queue_t queue)
 
 }
 
+// function to add a new node to the end of the queue
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
 	if (queue == NULL || data == NULL) {
-		//printf("failed\n");
 		return -1;
 	}
 	else {
+		// allocate memory for the new node, update the queue parameters and return 0
 		struct Node* new = malloc(sizeof(struct Node));
 		if (new == NULL) {
 			return -1;
 		}
+		// set the node's data to data and its next pointer to nill
 		new->data = data;
 		new->next = NULL;
+		// if there is nothing in the queue, we set the queue's head and end to new node
 		if (queue->head == NULL) {
 			queue->head = new;
 			queue->end = new;
 		}
+		// else, we set the tail of the queue to point to the new node
 		else {
 			queue->end->next = new;
 			queue->end = new;
 		}
+		// increment queue length
 		queue->len++;
 		return 0;
 	}
 }
 
+// function to remove the node at the head of the queue
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
 	if (queue == NULL || data == NULL || queue->len == 0) {
-		//printf("failed\n");
 		return -1;
 	}
+	// set the data to be returned as the data element of the head node
 	*data = queue->head->data;
 	struct Node* temp = queue->head;
+	// update the head of the queue to point to the next node
 	queue->head = queue->head->next;
 
 	if (queue->head == NULL)
 		queue->end = NULL;
 
+	// free the memory allocated to the previous head node
 	free(temp);
+	// decrement length
 	queue->len--;
 	return 0;
 }
-//x -> x -> x
+// function to delete a node from the queue
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+
 	if (queue == NULL || queue->head == NULL || data == NULL) {
 		return -1;
 	}
@@ -98,6 +107,7 @@ int queue_delete(queue_t queue, void *data)
 	struct Node* curr = queue->head;
 	struct Node* next = queue->head->next;
 
+	// handle case where queue has only one element
 	if (queue_length(queue) == 1) {
 		if (curr->data == data) {
 			free(curr);
@@ -109,6 +119,7 @@ int queue_delete(queue_t queue, void *data)
 		}
 	}
 
+	// iterate through the queue
 	while (next != NULL) {
 		if (curr->data == data) {
 			if (prev == NULL) { // delete head of queue
@@ -130,6 +141,7 @@ int queue_delete(queue_t queue, void *data)
 	return -1;
 }
 
+// function to iterate through the queue and apply a function to each node
 int queue_iterate(queue_t queue, queue_func_t func)
 {
 	if (queue == NULL || func == NULL) {
@@ -137,13 +149,14 @@ int queue_iterate(queue_t queue, queue_func_t func)
 	}
 	struct Node* temp = queue->head;
 	while (temp != NULL) {
-		//printf("iterate\n");
-		(*func)(queue, temp->data);
+		// apply the function to the node
+		(*func)(queue, temp->data); 
 		temp = temp->next;
 	}
 	return 0;
 }
 
+// function to return the length of the queue
 int queue_length(queue_t queue)
 {
 	if (queue == NULL) {
@@ -151,4 +164,3 @@ int queue_length(queue_t queue)
 	}
 	return queue->len;
 }
-
